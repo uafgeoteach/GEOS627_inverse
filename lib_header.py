a2e0f00c9ee41b2549006e8cd35d166486312723
Carl Tape <carltape@gmail.com>
2024-03-12 22:21:15 +0000

    add lib_header.py for default header

---
+++ lib_header.py
@@ -0,0 +1,13 @@
+# default libraries
+
+# basic python tools
+import matplotlib.pyplot as plt
+import numpy as np
+
+# these are for displaying matrices nicely
+from sympy import Matrix, init_printing
+from IPython.display import display
+from lib_geos import showmat
+
+import warnings
+warnings.filterwarnings('ignore')
